a945917de9af8ae2b4fb62edd9c8dc726c25410b
Meherunnesa Hossain Ibnath <97879002+ibnath1122@users.noreply.github.com>
2022-07-17 15:59:29 +0600

    Add files via upload

---
+++ Ibnath/src/ibnath/Ibnath.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ibnath;
+
+/**
+ *
+ * @author User
+ */
+public class Ibnath {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
